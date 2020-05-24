896146279ba56f47a452c39afab99f1083fdbab2
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-05-24 01:29:40 +0100

    add tiny delay to rule checking

--- src/content_script.ts
+++ src/content_script.ts
@@ -1,7 +1,18 @@
 import cookieMonsters from './cookie-monsters'
+import asyncFind from './utils/async-find'
+import delay from './utils/delay'
 
-const init = function init() {
-  const monsterMatch = cookieMonsters.find(async ({ rule }) => await rule())
+interface Monster {
+  rule: () => Promise<boolean>
+  handler: () => Promise<void>
+}
+
+const init = async function init() {
+  await delay(500)
+  const monsterMatch = (await asyncFind(
+    cookieMonsters,
+    async (monster: Monster): Promise<boolean> => monster.rule()
+  )) as Monster
 
   if (!monsterMatch) return
 
