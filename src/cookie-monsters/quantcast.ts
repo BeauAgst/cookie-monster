4046b005f16dde9f32c0e1d1108acc735c23f990
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-05-24 01:29:29 +0100

    handle 2 quantcast formats

--- src/cookie-monsters/quantcast.ts
+++ src/cookie-monsters/quantcast.ts
@@ -2,11 +2,24 @@ import getCookie from '../utils/get-cookie'
 import getHostnameFromString from '../utils/get-hostname-from-string'
 import $ from '../utils/select'
 import $$ from '../utils/wait-for-el-by-innertext'
+import waitForElByInnerText from '../utils/wait-for-el-by-innertext'
 
 /*
   https://www.quantcast.com/
   https://iabeurope.eu/
 */
+
+const getCookieConfiguration = function getCookieConfiguration(): string {
+  const scripts = document.querySelectorAll('script')
+  const library = Array.from(scripts).find(
+    ({ src }) => getHostnameFromString(src) === 'static.quantcast.mgr.consensu.org'
+  )
+
+  if (!library) throw new Error('Unable to find quantcast configuration')
+
+  const { pathname } = new URL(library.src)
+  return pathname
+}
 
 const openMoreOptions = async function openMoreOptions(): Promise<void> {
   const button = await $$('.qc-cmp-button', document, 'more options')
@@ -23,19 +36,41 @@ const savePreferences = async function savePreferences(): Promise<void> {
   button.click()
 }
 
+const handlePopupConfiguration = async function handlePopupConfiguration(): Promise<void> {
+  await $('.qc-cmp-ui', {
+    maxAttempts: 10,
+    timeout: 100,
+  })
+
+  await $('.qc-cmp-ui', { maxAttempts: 10, timeout: 100 })
+  await openMoreOptions()
+  await clickRejectAll()
+  await savePreferences()
+}
+
+const handleBannerConfiguration = async function handleBannerConfiguration(): Promise<void> {
+  const banner = await $('#qcCmpUi')
+  const button = await waitForElByInnerText('.qc-cmp-button', banner, 'I do not accept')
+  button.click()
+}
+
 const handler = async function handler() {
   if (getCookie('euconsent')) return
 
   try {
-    await $('.qc-cmp-ui', {
-      maxAttempts: 10,
-      timeout: 100,
-    })
+    const configuration = getCookieConfiguration()
 
-    await $('.qc-cmp-ui', { maxAttempts: 10, timeout: 100 })
-    await openMoreOptions()
-    await clickRejectAll()
-    await savePreferences()
+    console.log(configuration)
+
+    if (configuration.includes('cmpui-banner')) {
+      await handleBannerConfiguration()
+      return
+    }
+
+    if (configuration.includes('cmpui-popup')) {
+      await handlePopupConfiguration()
+      return
+    }
   } catch (error) {
     console.log(error.message)
   }
@@ -44,7 +79,7 @@ const handler = async function handler() {
 const rule = function rule(): boolean {
   const scripts = document.querySelectorAll('script')
   if (!scripts) return false
-  return !!Array.from(scripts).find(({ src }) => getHostnameFromString(src) === 'quantcast.mgr.consensu.org')
+  return !!Array.from(scripts).find(({ src }) => getHostnameFromString(src) === 'static.quantcast.mgr.consensu.org')
 }
 
 export default { handler, rule }
