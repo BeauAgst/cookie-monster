4ff9fccb2e0084616ac7ec24c4c465c155e43a34
Beau August <10343831+BeauAgst@users.noreply.github.com>
2020-05-24 13:18:53 +0100

    housekeeping

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import typescript from 'rollup-plugin-typescript'
 import resolve from 'rollup-plugin-node-resolve'
 import babel from 'rollup-plugin-babel'
 import { terser } from 'rollup-plugin-terser'
+import copy from 'rollup-plugin-copy'
 
 const plugins = [
   typescript({
@@ -18,19 +19,14 @@ const plugins = [
   }),
 ]
 
-export default [
-  {
-    input: 'src/background.ts',
-    output: {
-      dir: 'dist',
-    },
-    plugins,
+export default {
+  input: 'src/content_script.ts',
+  output: {
+    dir: 'dist',
   },
-  {
-    input: 'src/content_script.ts',
-    output: {
-      dir: 'dist',
-    },
-    plugins,
-  },
-]
+  plugins: plugins.concat(
+    copy({
+      targets: [{ src: 'public/*', dest: 'dist' }],
+    })
+  ),
+}
